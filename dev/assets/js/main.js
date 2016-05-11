320323a478221755b65bb35ab64a7663d125d3e9
Cherry Wang <v-cherrw@microsoft.com>
2016-05-10 22:13:25 -0700

    no 3rd party framework mockup...just for fun

---
+++ dev/assets/js/main.js
@@ -0,0 +1,9 @@
+require.config({
+    paths: {
+        jquery: [
+            'https://code.jquery.com/jquery-2.2.3.min',
+            //If the CDN location fails, load from this location
+            'lib/jquery-2.2.3.min'
+        ]
+    }
+});
